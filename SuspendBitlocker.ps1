c8a1f9681668f7953efe48c98bb8d69d91dc2d78
FaronicsProServices <proservices@faronics.com>
2024-11-21 13:27:58 +0530

    Update SuspendBitlocker.ps1

--- SuspendBitlocker.ps1
+++ SuspendBitlocker.ps1
@@ -1,2 +1,2 @@
-# Temporarily disable BitLocker protection on the C: drive, with protection resuming after the next reboot
+# Temporarily disable BitLocker protection on the C: drive or any other bitlocker suspended drive, with protection resuming after the next reboot
 manage-bde -protectors -Disable C: -RebootCount 0
