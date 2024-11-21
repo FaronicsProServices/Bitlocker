1e26b08185c30b389e104f3141d1c760f0a1e0ed
FaronicsProServices <proservices@faronics.com>
2024-11-21 13:28:52 +0530

    Update SuspendBitlocker.bat

--- SuspendBitlocker.bat
+++ SuspendBitlocker.bat
@@ -1,2 +1,2 @@
-REM Temporarily disable BitLocker protection on the C: drive or any other bitlocker suspended drive, with protection resuming after the next reboot
+REM Temporarily disable BitLocker protection on the C: drive or any other bitlocker encrypted drive, with protection resuming after the next reboot
 manage-bde -protectors -Disable C: -RebootCount 0
